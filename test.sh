02ed494f921d63748e7a8c2653699d83cc4d6d39
facumolina <facu.molina7@gmail.com>
2015-09-09 03:00:46 -0300

    Update scripts

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ cd classes
 export CLASSPATH=""
 for file in `ls ../lib/`; do export CLASSPATH=$CLASSPATH:../lib/$file; done
 java -cp $CLASSPATH:. org.junit.runner.JUnitCore CTdsScannerTest
-
+java -cp $CLASSPATH:. org.junit.runner.JUnitCore CTdsParserTest
